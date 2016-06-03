3a098691a46d1770871132031ea5fa531ba93fd8
Shiyao <liushiyao@Shiyao.local>
2016-06-02 22:11:32 -0500

    Some components

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
+
+var TodoApp = React.createClass({
+	getInitialState: function() {
+		return {
+			todos: [
+				{
+					id: 1,
+					text: 'Walk the dog'
+				},  {
+					id: 2,
+					text: 'Clean the yard'
+				}, {
+					id: 3,
+					text: 'Throw the garbage'
+				}, {
+					id: 4,
+					text: 'cook dinner'
+				}
+			]
+		};
+	},
+
+	handleAddTodo: function(text) {
+		alert('New todo: ' + text);
+	},
+
+	render: function() {
+		var {todos} = this.state;
+
+		return (
+				<div>
+					<TodoList todos={todos}/>
+					<AddTodo onAddTodo={this.handleAddTodo}/>
+				</div>
+			);
+	}
+});
+
+module.exports = TodoApp;
