f0c6d0add610f674b44ea164beda8d9f18d42030
Shiyao <liushiyao@Shiyao.local>
2016-06-02 22:46:49 -0500

    change id to use uuid to avoid confliction

---
+++ playground/spread.js
@@ -0,0 +1,12 @@
+// function add(a, b) {
+// 	return a + b;
+// }
+
+// var toAdd = [9, 5];
+// console.log(add(...toAdd));
+
+var groupA = ['Jen', 'Cory'];
+var groundB = ['Vikram'];
+var final = [3, ...groupA];
+
+console.log(final);
