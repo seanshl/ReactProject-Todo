5e8c7d246a6a5cb539dea4809c13fda8d2330282
Shiyao <liushiyao@Shiyao.local>
2016-06-19 15:47:37 -0500

    Add logout

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -4,7 +4,7 @@ try {
 
 	var config = {
 		apiKey: process.env.API_KEY,
-		authDomain: process.env.AUTO_DOMAIN,
+		authDomain: process.env.AUTH_DOMAIN,
 		databaseURL:  process.env.DATABASE_URL,
 		storageBucket: process.env.STORAGE_BUCKET
 	};
