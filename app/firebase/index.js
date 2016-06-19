bbea2be28083809acff1df637de5c104f316f518
Shiyao <liushiyao@Shiyao.local>
2016-06-19 10:32:08 -0500

    Deploy firebase depends on environment

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,10 +3,10 @@ import firebase from 'firebase';
 try {
 
 	var config = {
-		apiKey: "AIzaSyBKbQ_TmPm0V3GlfrnCBsTfvMqg0gJt5Mg",
-		authDomain: "react-todo-app-b2fae.firebaseapp.com",
-		databaseURL: "https://react-todo-app-b2fae.firebaseio.com",
-		storageBucket: "react-todo-app-b2fae.appspot.com",
+		apiKey: process.env.API_KEY,
+		authDomain: process.env.AUTO_DOMAIN,
+		databaseURL:  process.env.DATABASE_URL,
+		storageBucket: process.env.STORAGE_BUCKET
 	};
 
 
